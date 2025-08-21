f3f421b37d554ce8661e4c06cdee139308e5f8ba
Ben Carley <ben.carley@featurespace.co.uk>
2025-08-21 13:07:47 +0100

    initial commit

---
+++ pyrabbit_test/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from pyrabbit-test!")
+
+
+if __name__ == "__main__":
+    main()
---
+++ pyrabbit_test/plugin.py
@@ -0,0 +1,39 @@
+from typing import TYPE_CHECKING
+
+import astroid
+
+import os
+from pylint.checkers import BaseChecker
+
+if TYPE_CHECKING:
+    from pylint.lint import PyLinter
+
+
+class SimpleCheck(BaseChecker):
+    """Simple"""
+
+    name = "simple-test-check"
+    msgs = {"W6900": ("Test", "test-check", "Just a test")}
+    options = (
+        (
+            "fake-option",
+            {
+                "default": False,
+                "type": "yn",  # EDIT: bool here was not allowed
+                "metavar": "<y or n>",
+                "help": "Some fake option",
+            },
+        ),
+    )
+
+    def visit_import(self, _: astroid.nodes.Import) -> None:
+        breakpoint()  # NOT HIT!
+
+
+def register(linter: "PyLinter") -> None:
+    """This required method auto registers the checker during initialization.
+
+    :param linter: The linter to register the checker to.
+    """
+    print(os.environ)
+    linter.register_checker(SimpleCheck(linter))
